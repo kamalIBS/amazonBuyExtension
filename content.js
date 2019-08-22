112221c23313d8b097b8805a3480857d9d1ac8ab
kamalIBS <kamal.sharma@interbitsolutions.com>
2019-08-22 19:01:47 +0530

    Added changes for address verification and offer listing.

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
 // check if user is logged in
-if(document.getElementById("nav-al-signin").length!=0){
+let _loginEl = document.getElementById("nav-al-signin");
+if(_loginEl && _loginEl.innerHTML.length!=0){
     alert('Please login before placing order');
 }else{
-    // check if any any offer exists
-    if($("div.olpOffer").length==0 || $("div.olpOffer")==undefined){
-        alert('No offers available');
+    if (window.location.href.indexOf("offer-listing") > -1) {
+        let _offerEl = jQuery('div.olpOffer');
+        // check if any any offer exists
+        if(_offerEl.length==0 || _offerEl==undefined){
+            alert('No offers available');
+        }
     }
 }
 
@@ -14,18 +18,22 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
     if ((msg.from === 'popup') && (msg.subject === 'ErrorMsg')) {
         alert(msg.textMessage);
     }else if((msg.from === 'popup') && (msg.subject === 'elementCheck')){
-        if(msg.elementCount==0){
-            chrome.storage.sync.get('payLoad',function(requestPayLoad){
-                let myObj = requestPayLoad.payLoad.shippingAddress;
-                document.getElementById("enterAddressFullName").value = myObj.name;
-                document.getElementById("enterAddressAddressLine1").value = myObj.street1;
-                document.getElementById("enterAddressAddressLine2").value = myObj.street2;
-                document.getElementById("enterAddressCity").value = myObj.cityName;
-                document.getElementById("enterAddressStateOrRegion").value = myObj.stateOrProvince;
-                document.getElementById("enterAddressPostalCode").value = myObj.postalCode;
-                document.getElementById("enterAddressCountryCode").value = 'US';
-                document.getElementById("enterAddressPhoneNumber").value = myObj.phone;
-            });
+        if(document.getElementById("nav-al-signin")==null){
+            if (window.location.href.indexOf("addressselect") > -1 || window.location.href.indexOf("pay-select") > -1) {
+                chrome.storage.sync.get('payLoad',function(requestPayLoad){
+                    let myObj = requestPayLoad.payLoad.shippingAddress;
+                    document.getElementById("enterAddressFullName").value = myObj.name;
+                    document.getElementById("enterAddressAddressLine1").value = myObj.street1;
+                    document.getElementById("enterAddressAddressLine2").value = myObj.street2;
+                    document.getElementById("enterAddressCity").value = myObj.cityName;
+                    document.getElementById("enterAddressStateOrRegion").value = myObj.stateOrProvince;
+                    document.getElementById("enterAddressPostalCode").value = myObj.postalCode;
+                    document.getElementById("enterAddressCountryCode").value = 'US';
+                    document.getElementById("enterAddressPhoneNumber").value = myObj.phone;
+                });
+            }else{
+                alert('Goto the address page then fill-in address');
+            }
         }else{
             alert('Please login before filling Address');
         }
--- popup.js
+++ popup.js
@@ -12,24 +12,18 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('fillAddress');
     checkPageButton.addEventListener('click', function() {
-        chrome.tabs.executeScript( null, {
-            code:'var elLength = document.getElementById("nav-al-signin").innerHTML.length; elLength'
-        },
-        function(results){
-            chrome.tabs.query({
-                active: true,
-                currentWindow: true
-            }, tabs => {
-                chrome.tabs.sendMessage(
-                    tabs[0].id,
-                    {
-                        from: 'popup',
-                        subject: 'elementCheck',
-                        elementCount: results,
-                        textMessage:''
-                    },
-                );
-            });
-        } );
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, tabs => {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                    from: 'popup',
+                    subject: 'elementCheck',
+                    textMessage:''
+                },
+            );
+        });
     });
 });
